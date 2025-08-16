61c9783998621a85a5c8fc25fe53cdbae8b0a414
Mina <jugj@users.noreply.github.com>
2025-08-16 09:41:03 +0200

    Änderungen von Mina am 2025-08-16 09:41:03

---
+++ Assets/spielerbewegung.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spielerbewegung : MonoBehaviour
+{
+    public float Geschwindigkeit=5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if(Input.GetKey("space")){
+        transform.Translate(Vector2.up * Time.deltaTime * Geschwindigkeit);}
+
+        if(Input.GetKey("a")){
+        transform.Translate(Vector2.left * Time.deltaTime * Geschwindigkeit);}
+
+        if(Input.GetKey("d")){
+        transform.Translate(Vector2.right * Time.deltaTime * Geschwindigkeit);
+        }
+
+        
+        
+    }
+}
