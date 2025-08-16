cf15cea930fbd836031b98a10efa4271b79c21f0
Jakob <jugj@users.noreply.github.com>
2025-08-16 10:34:22 +0200

    Änderungen von Jakob am 2025-08-16 10:34:22

--- Assets/spielerbewegung.cs
+++ Assets/spielerbewegung.cs
@@ -14,17 +14,21 @@ public class spielerbewegung : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        if(Input.GetKey("space")){
+        if(Input.GetKey("up")){
         transform.Translate(Vector2.up * Time.deltaTime * Geschwindigkeit);}
 
-        if(Input.GetKey("a")){
+        if(Input.GetKey("left")){
         transform.Translate(Vector2.left * Time.deltaTime * Geschwindigkeit);}
 
-        if(Input.GetKey("d")){
+        if(Input.GetKey("right")){
         transform.Translate(Vector2.right * Time.deltaTime * Geschwindigkeit);
         }
-
-        
+        if(Input.GetKey("shift + left")){
+        transform.Translate(Vector2.left * Time.deltaTime * 10);
+        }
+        if(Input.GetKey("shift + right")){
+        transform.Translate(Vector2.right * Time.deltaTime * 10);
+        }
         
     }
 }
